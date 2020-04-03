35a13c7d0ca6924159bc21f9aa46b0423e810418
Ozhan Yilmaz <ozhanyilmaz@gmail.com>
2020-04-03 20:54:16 +0200

    2nd

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
 import React from "react";
 import "./App.css";
+const nums = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
+const ops = ["/", "*", "-", "+", "="];
+
 const App = () => (
   <div className="calculator">
     <div id="display" className="display">
-      125
+      <div>1500 </div>
+      <div className="nums-container">
+        {nums.map(num => (
+          <button className={`dark-grey ${num === 0 && "big-h"}`} key={num}>
+            {num}
+          </button>
+        ))}
+      </div>
+      <div classname="ops-container">
+        {ops.map(op => (
+          <button className="orange" key={op}>
+            {op}
+          </button>
+        ))}
+      </div>
     </div>
-    <button>1</button>
-    <button>2</button>
-    <button>3</button>
-    <button>4</button>
-    <button>1</button>
-    <button>2</button>
-    <button>3</button>
-    <button>4</button>
-    <button>1</button>
-    <button>2</button>
-    <button>3</button>
-    <button>4</button>
-    <button>1</button>
-    <button>2</button>
-    <button>3</button>
-    <button>4</button>
-    <button>1</button>
-    <button>2</button>
-    <button>3</button>
-    <button>4</button>
   </div>
 );
 
